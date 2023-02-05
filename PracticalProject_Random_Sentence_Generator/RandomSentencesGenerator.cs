2ba29a39313143864bd6fb3fd80ecc393aa5cd9a
nikolaK78 <115399438+nikolaK78@users.noreply.github.com>
2023-02-05 16:25:42 +0200

    Fixed some grammar issues, added new examples

--- PracticalProject_Random_Sentence_Generator/RandomSentencesGenerator.cs
+++ PracticalProject_Random_Sentence_Generator/RandomSentencesGenerator.cs
@@ -1,9 +1,9 @@
-﻿string[] names = { "Peter", "Michell", "Jane", "Steve" };
-string[] places = { "Sofia", "Plovdiv", "Varna", "Bugars" };
-string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
-string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
-string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
-string[] details = { "near the river", "at home", "in the park" };
+﻿string[] names = { "Peter", "Michell", "Jane", "Steve", "Matt", "Michael", "John", "Martin", "Ivan", "Valq", "Aleksandra", "Nikoleta" };
+string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas", "Ruse", "Veliko Turnovo", "Stara Zagora", "Qmbol", "Sozopol", "Botevgrad", "Blagoevgrad" };
+string[] verbs = { "eats", "holds", "sees", "plays with", "brings", "sleeps", "decides", "expects", "collects" };
+string[] nouns = { "stones", "cake", "apples", "laptop", "bikes", "pears", "headphones", "remote control", "PC", "cup" };
+string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly", "carelessly", "bravely", "cheerfully", "rarely", "annualy" };
+string[] details = { "near the river", "at home", "in the park", "at the airport", "in the bedroom", "near the school", "at the restaurant" };
 
 string GetRandomWord(string[] words)
 {
@@ -12,6 +12,7 @@ string GetRandomWord(string[] words)
     string word = words[randomIndex];
     return word;
 }
+
 Console.WriteLine("Hello, this is your first random-generated sentence: ");
 while (true)
 {
@@ -26,5 +27,6 @@ while (true)
     string sentence = $"{who} {action} {randomDetails}.";
     Console.WriteLine(sentence);
     Console.WriteLine("Click [Enter] to generate a new one.");
+    Console.WriteLine();
     Console.ReadKey();
 }
